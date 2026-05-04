0ba891ed4b2a02f580467d5169a2e0e66f7890e4
Ciolocoi Ioan-Laurentiu <laurentiuciolocoi@yahoo.com>
2026-05-04 23:39:37 +0300

    Created the monitor_reports

---
+++ monitor_reports.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define PID_FILE ".monitor_pid"
+
+void handle_sigusr1(int sig) {
+    (void)sig;
+    char *msg = "[monitor] New report added to a district.\n";
+    write(STDOUT_FILENO, msg, strlen(msg));
+}
+
+void handle_sigint(int sig) {
+    (void)sig;
+    char *msg = "[monitor] Shutting down.\n";
+    write(STDOUT_FILENO, msg, strlen(msg));
+    unlink(PID_FILE);
+    _exit(0);
+}
+
+int main(int argc, char *argv[]) {
+
+    int fd = open(PID_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd < 0) {
+        perror("open .monitor_pid");
+        return 1;
+    }
+
+    char pid_buf[32];
+    int len = snprintf(pid_buf, sizeof(pid_buf), "%d\n", getpid());
+    close(fd);
+
+    printf("[monitor] Started with PID %d\n", getpid());
+
+    struct sigaction sa_usr1, sa_int;
+
+    sa_usr1.sa_handler = handle_sigusr1;
+    sigemptyset(&sa_usr1.sa_mask);
+    sa_usr1.sa_flags = SA_RESTART;
+    sigaction(SIGUSR1, &sa_usr1, NULL);
+
+    sa_int.sa_handler = handle_sigint;
+    sigemptyset(&sa_int.sa_mask);
+    sa_int.sa_flags = 0;
+    sigaction(SIGINT, &sa_int, NULL);
+
+    while (1) {
+        pause();
+    }
+
+    return 0;
+}
